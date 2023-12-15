a6b170f78987f43f23fac8756f1feeeb7a315275
d0ra-th3-3xpl0ra <90798127+d0ra-1h3-3xpl0ra@users.noreply.github.com>
2023-12-15 17:36:52 +0100

    add ts deps and configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+import "@nomicfoundation/hardhat-chai-matchers" ;
+import "@nomiclabs/hardhat-ethers" ;
+import '@openzeppelin/hardhat-upgrades' ;
+import 'hardhat-dependency-compiler' ;
+
+const config: HardhatUserConfig = { 
+    networks: {
+      hardhat: {
+        allowUnlimitedContractSize: true
+      }
+    },
+    solidity: {
+      compilers: [
+        { version: "0.8.16" },
+        { version: "0.7.6" },
+        { version: "0.6.6" }
+      ]
+    },
+    dependencyCompiler: {
+      paths: [
+        '@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol',
+        '@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol',
+        'solmate/src/tokens/WETH.sol',
+      ],
+    }
+}
+
 
 export default config;
